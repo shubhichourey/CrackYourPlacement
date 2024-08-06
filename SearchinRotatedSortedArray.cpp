5a2565ab55545a45ee5846357fe04ff9edf2cea5
Shubhi Chourey <143348496+shubhichourey@users.noreply.github.com>
2024-08-06 22:53:26 +0530

    day 14

---
+++ SearchinRotatedSortedArray.cpp
@@ -0,0 +1,35 @@
+/*There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+         int l = 0;
+    int r = nums.size() - 1;
+
+    while (l <= r) {
+      const int m = (l + r) / 2;
+      if (nums[m] == target)
+        return m;
+      if (nums[l] <= nums[m]) {  // nums[l..m] are sorted.
+        if (nums[l] <= target && target < nums[m])
+          r = m - 1;
+        else
+          l = m + 1;
+      } else {  // nums[m..n - 1] are sorted.
+        if (nums[m] < target && target <= nums[r])
+          l = m + 1;
+        else
+          r = m - 1;
+      }
+    }
+
+    return -1;
+    }
+};
