2bfedd54d11a6ccb8af8389cb80c334716b77021
Shubhi Chourey <143348496+shubhichourey@users.noreply.github.com>
2024-08-08 22:41:31 +0530

    day 16

---
+++ SortList.cpp
@@ -0,0 +1,27 @@
+/*Given the head of a linked list, return the list after sorting it in ascending order.
+*/
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+         if(!head) return head;
+        vector<int> arr;
+        ListNode* cur = head;
+        
+        while(cur){
+            arr.push_back(cur->val);
+            cur = cur->next;
+        }
+        
+        sort(arr.begin(), arr.end());
+        
+        cur = head;
+        int i = 0;
+        while(cur){
+            cur->val = arr[i++];
+            cur = cur->next;
+        }
+        
+        return head;
+    }
+};
