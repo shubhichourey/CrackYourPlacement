f72c34aee6191aaa84b4bf656ac9ed407de879fd
Shubhi Chourey <143348496+shubhichourey@users.noreply.github.com>
2024-08-10 22:23:15 +0530

    day 18

---
+++ OnlineStockSpan.cpp
@@ -0,0 +1,23 @@
+/*
+Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+
+The span of the stock's price in one day is the maximum number of consecutive days (starting from that day and going backward) for which the stock price was less than or equal to the price of that day.
+*/
+
+class StockSpanner {
+public:
+ stack<pair<int,int>> st;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span=1;
+        while(!st.empty()&&st.top().first<=price){
+            span+=st.top().second;
+            st.pop();
+        }
+        st.push({price,span});
+        return span;
+    }
+};
