5156cd4bcec66b8ab6396953fa228b05e64fe4ff
Shubhi Chourey <143348496+shubhichourey@users.noreply.github.com>
2024-08-11 18:27:56 +0530

    day 19

---
+++ TheCelebrityProblem.cpp
@@ -0,0 +1,55 @@
+/*A celebrity is a person who is known to all but does not know anyone at a party. A party is being organized by some people.  A square matrix mat is used to represent people at the party such that if an element of row i and column j is set to 1 it means ith person knows jth person. You need to return the index of the celebrity in the party, if the celebrity does not exist, return -1.
+*/
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& mat) {
+        // code here
+         int n=mat.size();
+        int a=0;
+        int b=n-1;
+        while(a<b){
+            if(mat[a][b])
+            a++;
+            else
+            b--;
+        }
+        for(int i=0;i<n;i++){
+            if((i!=a) && (mat[a][i] || !mat[i][a]))
+            return -1;
+        }
+        return a;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int> > M(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> M[i][j];
+            }
+        }
+        Solution ob;
+        cout << ob.celebrity(M) << endl;
+    }
+}
+
+// } Driver Code Ends
