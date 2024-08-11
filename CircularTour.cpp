f221c57fee450e09aed69cd2a901ae87c4be9513
Shubhi Chourey <143348496+shubhichourey@users.noreply.github.com>
2024-08-11 17:47:38 +0530

    day 19

---
+++ CircularTour.cpp
@@ -0,0 +1,74 @@
+/*
+Suppose there is a circle. There are N petrol pumps on that circle. You will be given two sets of data.
+1. The amount of petrol that every petrol pump has.
+2. Distance from that petrol pump to the next petrol pump.
+Find a starting point where the truck can start to get through the complete circle without exhausting its petrol in between.*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};
+
+
+// } Driver Code Ends
+/*
+The structure of petrolPump is 
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};*/
+
+/*You are required to complete this method*/
+class Solution{
+  public:
+  
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(petrolPump p[],int n)
+    {
+       //Your code here
+       int start=0,deficit=0;
+    int surplus=0;
+   
+   for(int i=0;i<n;i++)
+   {
+       surplus+=p[i].petrol-p[i].distance;
+       
+       if(surplus<0){  
+           start=i+1;
+           deficit+=surplus; 
+           surplus=0;           
+       }
+   }
+   
+   return(surplus+deficit>0)?start:-1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        petrolPump p[n];
+        for(int i=0;i<n;i++)
+            cin>>p[i].petrol>>p[i].distance;
+        Solution obj;
+        cout<<obj.tour(p,n)<<endl;
+    }
+}
+
+// } Driver Code Ends
